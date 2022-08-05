26fa1fc944129c63fb976f803fe0247563e3838e
mollikacomputer <mollikacomputer3@gmail.com>
2022-08-06 00:24:34 +0600

    Login form created

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -4,6 +4,11 @@ const Login = () => {
     return (
         <div>
             <h2 className='text-3xl' > Login</h2>
+            <form>
+            <input type="email" name='email' placeholder="email" class="input input-bordered w-full max-w-xs" /><br/><br/>
+            <input type="password" name='password' placeholder="email" class="input input-bordered w-full max-w-xs" /><br/><br/>
+            <input type="submit" className='btn' value="Add User" />
+            </form>
         </div>
     );
 };
